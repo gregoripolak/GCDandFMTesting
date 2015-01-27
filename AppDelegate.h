7abad7a957918248e6e89e33405afd9c06a020da
gregoripolak <gregoripolak@gmail.com>
2015-01-27 21:17:55 +0200

    Working app :)

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MultiThreadingAndFileManagerTesting
+//
+//  Created by Gil Polak on 1/27/15.
+//  Copyright (c) 2015 Gregori Polak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
